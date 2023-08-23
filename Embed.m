b3ec326c327b5a4babd7254e9575ebea27758042
Jacob Törngren <jacob.torngren@hotmail.com>
2023-08-23 15:12:01 +0200

    Works now

--- Embed.m
+++ Embed.m
@@ -6,16 +6,18 @@ fs = 44100;             % Sampling frequency
 im = double(im2gray(image_raw));
 im = im - min(im, [],"all"); im = im/max(im,[],"all");
 
-target_win_len = floor(sound_len * fs / size(im, 1));
 orig_win_len = size(im, 1);
+half_target_win_len = floor(sound_len*fs / orig_win_len / 2);
 num_wins = size(im, 2);
 
-output = zeros(target_win_len * num_wins, 1);
+f_orig = linspace(0,fs/2,orig_win_len);
+f_stretch = linspace(0, fs/2, half_target_win_len);
+
+output = zeros(2*half_target_win_len * num_wins, 1);
 for i = 0:num_wins-1
-    % win = bartlett(orig_win_len);
-    snippet = flip(im(:,i+1)); %.*win
-    sig = ifft(snippet, target_win_len);
-    output(1+i*target_win_len:(i+1)*target_win_len) = real(sig);
+    snippet = interp1(f_orig, im(:,i+1), f_stretch,"nearest");
+    sig = ifft(flip(snippet), 2*half_target_win_len);
+    output(1+i*2*half_target_win_len:(i+1)*2*half_target_win_len) = real(sig);
 end
 output = normalize(output,'range')*.8;
 
