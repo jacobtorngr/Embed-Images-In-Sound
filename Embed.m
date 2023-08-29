9555f4e6bf0b0c3dd12ffca2db95e8bca91cc4f4
Jacob Törngren <jacob.torngren@hotmail.com>
2023-08-29 18:26:13 +0200

    Added rcond check

--- Embed.m
+++ Embed.m
@@ -1,8 +1,8 @@
 % Parameters
 image_raw = imread("Example_Image.png");
-sound_len = 5;         % Desired length of sound clip, seconds
+sound_len = 20;         % Desired length of sound clip, seconds
 fs = 44100;             % Sampling frequency
-gain = .4;              % Audio volume
+gain = .8;              % Maximum amplitude
 stochastic = true;      % Toggle between stochastic or deterministic signal
 
 bins = min(floor(sqrt(sound_len*fs)), size(image_raw,1));
@@ -19,6 +19,7 @@ f_orig = linspace(0,fs/2,orig_win_len);
 f_stretch = linspace(0, fs/2, half_target_win_len);
 
 output = zeros(2*half_target_win_len * num_wins, 1);
+j=0;
 for i = 0:num_wins-1
     snippet = interp1(f_orig, im(:,i+1), f_stretch,"nearest");
 
@@ -30,19 +31,24 @@ for i = 0:num_wins-1
             acs = real(cacs(1:half_target_win_len));
             
             R = toeplitz(acs(1:end-1));
-            r = acs(2:end)';
-            coeff = R\-r;
-            d02 = dot([1; coeff], snippet);
-            w = randn(1,2*half_target_win_len);
-            sig = filter(coeff, d02, w);
-            sig = (2*normalize(sig,'range')-1)*gain;
+            if rcond(R) < 1e-4
+                j = j+1;
+                % Use previous column
+            else
+                r = acs(2:end)';
+                coeff = R\-r;
+                d02 = dot([1; coeff], snippet);
+                w = randn(1,2*half_target_win_len);
+                sig = filter(coeff, d02, w);
+                % sig = (2*normalize(sig,'range')-1)*gain;
+            end
     end
     output(1+i*2*half_target_win_len:(i+1)*2*half_target_win_len) = real(sig);
 end
 
-if ~stochastic
+% if ~stochastic
     output = (2*normalize(output,'range')-1)*gain;
-end
+% end
 
 audiowrite("output.wav",output,fs);
 
